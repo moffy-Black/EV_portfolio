8ab24bd87cb5c797fc31588e1bae957bd5087f00
moffy-Black <black.river.emperor@i.softbank.jp>
2021-04-18 18:38:51 +0900

    start firebase

--- app/env.py
+++
@@ -1,8 +0,0 @@
-import os
-
-APP_ENV = os.environ.get('APP_ENV')
-
-DB_USER = os.environ.get('MYSQL_USER')
-DB_PASSWORD = os.environ.get('MYSQL_PASSWORD')
-DB_HOST = os.environ.get('MYSQL_HOST')
-DB_NAME = os.environ.get('MYSQL_DATABASE')
--- app/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from database import Base # noqa: F401
-
-from .company import Company # noqa: F401
-from .user import User # noqa: F401
