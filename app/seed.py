23a6be07aadcfceb4c897d5ac01afe6746a74ead
moffy-Black <black.river.emperor@outlook.jp>
2021-04-19 01:48:40 +0900

    delete: seed

--- app/seed.py
+++
@@ -1,6 +0,0 @@
-def detail(users_by_name):
-    detail_list = []
-    for key in users_by_name.keys():
-        detail = db.child("companies").child(key).get().val()
-        detail_list.append(detail)
-    return detail_list
